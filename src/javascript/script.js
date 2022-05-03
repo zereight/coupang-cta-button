e91b274022f3b1d1ba3df011db0a710dbb404591
TaehongKim <public.devhong@gmail.com>
2022-05-03 22:58:01 +0900

    야간모드 추가

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,4 +1,23 @@
-console.log('hello');
+window.onload = () => {
+  const body = document.querySelector('body');
+  const brightButton = document.querySelector('#bright-button');
+  const moonButton = document.querySelector('#moon-button');
 
-const test = [1, 2, 3].map((item) => item * 10);
-console.log(test);
+  const toggleTheme = (e) => {
+    const isDarkTheme = body.classList.contains('dark-theme');
+
+    if (isDarkTheme) {
+      moonButton.style.display = 'none';
+      brightButton.style.display = 'block';
+      body.classList.remove('dark-theme');
+    } else {
+      brightButton.style.display = 'none';
+      moonButton.style.display = 'block';
+      body.classList.add('dark-theme');
+    }
+  };
+
+  [brightButton, moonButton].map((btn) =>
+    btn.addEventListener('click', toggleTheme)
+  );
+};
