e9ac82332de132ec393685e5dfbb87886299821d
bluemiv <public.bluemiv@gmail.com>
2022-10-09 11:23:23 +0900

    feat: webpack, pug, scss 도입 #87

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,13 @@ const mode = process.env?.NODE_ENV?.toLowerCase() === 'prod' ? PROD : DEV;
 
 module.exports = {
   mode,
-  entry: './src/js/index.js',
+  entry: ['./src/js/index.js', './src/style/index.scss'],
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'index.js',
   },
+  plugins: [
+    // 컴파일 + 번들링 CSS 파일이 저장될 경로와 이름 지정
+    new MiniCssExtractPlugin({ filename: 'css/style.css' }),
+  ],
 };
