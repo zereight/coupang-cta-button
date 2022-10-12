342dd847be1601a5b9b6116420fdda3de4f71aa8
bluemiv <public.bluemiv@gmail.com>
2022-10-12 23:37:19 +0900

    feat: webpack, pug, scss 도입 #87

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,13 @@ import { defEventHandler, openTab } from './util-service';
 // initialize theme
 initTheme();
 
+const categoryCountRestyling = () => {
+  const countElements = document.querySelectorAll('.c_cnt');
+  countElements?.forEach((e) => {
+    e.innerText = e.innerText.replace(/\(|\)|\s/g, '');
+  });
+};
+
 document.addEventListener('DOMContentLoaded', (event) => {
   // Theme button event handler
   defEventHandler('#theme-btn', 'click', toggleTheme);
@@ -12,4 +19,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
   defEventHandler('#write-btn', 'click', () => openTab(WRITE_URL));
   // Manage button event handler
   defEventHandler('#manage-btn', 'click', () => openTab(MANAGE_URL));
+
+  // restyling category counts
+  categoryCountRestyling();
 });
