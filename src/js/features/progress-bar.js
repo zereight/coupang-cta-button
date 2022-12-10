7ae46a096ced785ddba55fc0594d531367a68917
taehongkim <public.bluemiv@gmail.com>
2022-12-10 20:27:48 +0900

    feat: 반응형 웹 설정 #108

--- src/js/features/progress-bar.js
+++ src/js/features/progress-bar.js
@@ -1,17 +1,11 @@
-import { throttle } from '../util-service';
-
 const changeProgressBarWidthByScroll = () => {
-  const handler = throttle(() => {
+  document.addEventListener('scroll', () => {
     const totalHeight = document?.scrollingElement?.scrollHeight;
     const currentY = window.scrollY + window.innerHeight;
     const rate = (currentY / totalHeight) * 100;
     const progressBar = document.querySelector('.progress-bar .fill');
     if (!progressBar?.style) return;
     progressBar.style.width = `${rate}%`;
-  });
-
-  document.addEventListener('scroll', () => {
-    handler();
   });
 };
 
